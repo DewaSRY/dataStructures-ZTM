e696ab85de15de33eda623701bc9347e5896ec22
DewaSRY <sdewa6645@gmail.com>
2023-08-04 14:02:27 +0800

    learn string

---
+++ src/Coding-Interviex/Strings/Longest-Substring-Whithout-Repeting-Character/LongestSubstringWithoutRepetition.test.ts
@@ -0,0 +1,38 @@
+import { describe, it, expect, beforeEach } from "vitest";
+import {
+  lengthOfLongestSubstring,
+  lengthOfLongestSubstringSecond,
+} from "./index";
+
+describe("type out two string", () => {
+  let suit: typeof lengthOfLongestSubstring;
+  const case1 = {
+    S1: "abccabb",
+    expect: 3,
+  };
+
+  describe("first method", () => {
+    beforeEach(() => {
+      suit = lengthOfLongestSubstring;
+    });
+    it.each([{ input1: case1.S1, expected: case1.expect }])(
+      "$input1 and $input2 should return $expected",
+      ({ input1, expected }) => {
+        const actual = suit(input1);
+        expect(actual).toBe(expected);
+      }
+    );
+  });
+  describe("first method", () => {
+    beforeEach(() => {
+      suit = lengthOfLongestSubstringSecond;
+    });
+    it.each([{ input1: case1.S1, expected: case1.expect }])(
+      "$input1 and $input2 should return $expected",
+      ({ input1, expected }) => {
+        const actual = suit(input1);
+        expect(actual).toBe(expected);
+      }
+    );
+  });
+});
---
+++ src/Coding-Interviex/Strings/Longest-Substring-Whithout-Repeting-Character/index.ts
@@ -0,0 +1,61 @@
+`
+Given a string, find the length of the longest 
+substring without repeating characters
+//tes case
+->is the substring contiguous ?
+=>yes, look for a substring not a subsequence
+->Dose case sensitivity meter?
+=>No, assume all characters in the string are lowercase.
+`;
+
+// Time: O(N^2);
+// Space: O(N)
+`
+=>Sliding window
+Form a window over some portion of sequential data, 
+then move that window throughout the data to capture 
+different parts of it
+`;
+export const lengthOfLongestSubstring = (s: string) => {
+  if (s.length <= 1) return s.length;
+  let longest = 0;
+  for (let left = 0; left < s.length; left++) {
+    let seenChars: { [k: string]: boolean } = {},
+      currentLength = 0;
+    for (let right = left; right < s.length; right++) {
+      const currentChar = s[right];
+      if (!seenChars[currentChar]) {
+        currentLength++;
+        seenChars[currentChar] = true;
+        longest = Math.max(longest, currentLength);
+      } else {
+        break;
+      }
+    }
+  }
+
+  return longest;
+};
+
+export const lengthOfLongestSubstringSecond = (s: string) => {
+  if (s.length <= 1) return s.length;
+
+  const seen: { [k: string]: number } = {};
+  let left = 0,
+    longest = 0;
+
+  for (let right = 0; right < s.length; right++) {
+    const currentChar = s[right];
+    const previouslySeenChar = seen[currentChar];
+
+    if (previouslySeenChar >= left) {
+      left = previouslySeenChar + 1;
+    }
+
+    seen[currentChar] = right;
+
+    longest = Math.max(longest, right - left + 1);
+  }
+
+  return longest;
+};
