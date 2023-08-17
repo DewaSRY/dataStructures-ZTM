04e3e54fec74a75488e18a91e0e436e8a5677243
DewaSRY <sdewa6645@gmail.com>
2023-08-17 19:21:53 +0800

    cooolll

---
+++ src/note/DataStrucuture/index.ts
@@ -0,0 +1,17 @@
+/**Data Structure
+ * Data Structure are collections of values, the relationship among
+ * them and function or operations that can be applied to the
+ * data
+ */
+/** Class
+ * Class is a bluePrint for creating object with pre-defined
+ * properties and methods
+ * ->Instance method is method connection on the class, method
+ *   works just like the function owned by the class
+ * ->Class Methods, class method works by define some function
+ *  inside class start withe 'static' keyWord, the static keyword
+ *  defines a static method for a class. static methods are
+ *  called without instantiating their class and cannot be called
+ *  through a class instance. static methods are often to create
+ *  utility function for an application
+ */
