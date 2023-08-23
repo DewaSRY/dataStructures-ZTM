0ccf692bd8ce9c3d669464667ebfd731dd1f5ce5
DewaSRY <sdewa6645@gmail.com>
2023-08-23 17:29:47 +0800

    test

--- src/Codding/BinaryTrees.ts
+++ src/Codding/BinaryTrees.ts
@@ -8,51 +8,106 @@ NOTE: The beginning portion builds our test case binary tree. Scroll down to the
 
 // ------- Code to generate our binary tree -------
 
-export class TreeNode {
-  public left: TreeNode | null = null;
-  public right: TreeNode | null = null;
+class Nodes {
+  public left: Nodes | null = null;
+  public right: Nodes | null = null;
   constructor(public value: number | null = null) {}
-  insert(values: (number | null)[]) {
-    const queue: TreeNode[] = [this];
-    let i = 0;
-    while (queue.length > 0) {
-      let current = queue.shift()!;
-      for (let side of [
-        "left" as unknown as TreeNode["left"],
-        "right" as unknown as TreeNode["right"],
-      ]) {
-        if (!side) return;
-        if (!current[side]) {
-          if (values[i] !== null) {
-            current[side] = new TreeNode(values[i]);
-          }
-          i++;
-          if (i >= values.length) return this;
-        }
-        if (current[side]) queue.push(current[side]);
-      }
+}
+export class BinaryTree {
+  public root: Nodes | null = null;
+
+  insertOne(val: number) {
+    const newNode = new Nodes(val);
+    if (!this.root) {
+      this.root = newNode;
+      return;
     }
+    insertNodeTree(this.root!, newNode);
     return this;
   }
 }
-
-const root = new TreeNode();
-root.insert([1, 1, 1, 1, null, null, null, 1, null, null, null, 1, null, null]);
-
-// ------- Code to generate our binary tree -------
-
-// ------- Our Solution -------
-export var maxDepth = function (node: TreeNode, currentDepth: number): number {
+const insertNodeTree = (node: Nodes, newNode: Nodes): Nodes | null => {
+  if (newNode.value! < node.value!) {
+    if (node.left === null) {
+      node.left = newNode;
+      return node;
+    } else if (node.left !== null) {
+      return insertNodeTree(node.left, newNode);
+    }
+  } else if (newNode.value! > node.value!) {
+    if (node.right === null) {
+      node.right = newNode;
+      return node;
+    } else if (node.right !== null) {
+      return insertNodeTree(node.right, newNode);
+    }
+  } else {
+    return null;
+  }
+  return null;
+};
+// // ------- Our Solution -------
+export var maxDepth = function (node: Nodes, currentDepth: number = 0): number {
   if (!node) {
     return currentDepth;
   }
-
   currentDepth++;
-
   return Math.max(
     maxDepth(node.right!, currentDepth),
     maxDepth(node.left!, currentDepth)
   );
 };
+/**Level order of binary tree
+ *
+ */
+export const levelOrder = function (root: Nodes) {
+  if (!root) return [];
+  const result: (number | number[])[] = [];
+  const queue = [root];
+  while (queue.length) {
+    const currentLevelValues: number[] = [];
+    let length = queue.length,
+      count = 0;
+    while (count < length) {
+      const currentNode = queue.shift()!;
+      currentLevelValues.push(currentNode.value!);
+      if (currentNode.left) {
+        queue.push(currentNode.left);
+      }
+      if (currentNode.right) {
+        queue.push(currentNode.right);
+      }
+      count++;
+    }
+    result.push(currentLevelValues);
+  }
+  return result;
+};
+/** Right side view of tree
+ * Given a binary tree, imagine you'r standing to the right
+ * of the tree.  Return an array of the values of the nodes
+ * you can see ordered from top to bottom
+ */
+const dfs = (node: Nodes, currentLevel = 0, result: number[]) => {
+  if (!node) return;
+  if (currentLevel >= result.length) {
+    result.push(node.value!);
+  }
 
-console.log(maxDepth(root, 0));
+  if (node.right) {
+    dfs(node.right, currentLevel + 1, result);
+  }
+
+  if (node.left) {
+    dfs(node.left, currentLevel + 1, result);
+  }
+};
+export const rightSideViewDFS = (root: Nodes) => {
+  const result: number[] = [];
+
+  dfs(root, 0, result);
+  return result;
+};
+/** Number of Nodes in Complete Tree
+ * Given a complete binary tree, count the number of nodes
+ */
