9bcd9eba31e0cc62c19399dbfcf89966c55d3303
DewaSRY <sdewa6645@gmail.com>
2023-08-29 15:06:47 +0800

    coll

--- src/DataStrucuture/DoublyLinkedLists.test.ts
+++ src/DataStrucuture/DoublyLinkedLists.test.ts
@@ -1,9 +1,9 @@
 import { it, describe, expect, beforeEach } from "vitest";
-import { DoublyLinkedLists, DoublyLinkedListSec } from "./DoublyLinkedLists";
+import { DoublyLinkedList } from "./DoublyLinkedLists";
 describe.skip("DoublyLinkedLists test suit", () => {
-  let suit: DoublyLinkedLists<number>;
+  let suit: DoublyLinkedList;
   beforeEach(() => {
-    suit = new DoublyLinkedLists<number>();
+    suit = new DoublyLinkedList();
   });
 
   describe("push should add new value on node and have the new node as a tail", () => {
@@ -22,9 +22,8 @@ describe.skip("DoublyLinkedLists test suit", () => {
   });
   describe("pop should remove the last item on Linked lists ", () => {
     beforeEach(() => {
-      suit.pushMuch([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+      [1, 2, 3, 4, 5, 6, 7, 8, 9].forEach((num) => suit.push(num));
     });
-
     it.each([
       { arr: [1, 2, 3], length: 6 },
       { arr: [1], length: 8 },
@@ -35,8 +34,10 @@ describe.skip("DoublyLinkedLists test suit", () => {
     });
   });
   describe("shift shift should remove the fist item change it with second lists", () => {
+    beforeEach(() => {
+      [1, 2, 3, 4].forEach((num) => suit.push(num));
+    });
     it("shift one time", () => {
-      suit.pushMuch([1, 2, 3, 4]);
       suit.shift();
       expect(suit.head?.value).toBe(2);
       expect(suit.head?.prev).toBe(null);
@@ -44,7 +45,6 @@ describe.skip("DoublyLinkedLists test suit", () => {
       expect(suit.get(2)?.prev?.value).toBe(2);
     });
     it("shift two time", () => {
-      suit.pushMuch([1, 2, 3, 4]);
       suit.shift();
       suit.shift();
       expect(suit.head?.value).toBe(3);
@@ -53,7 +53,6 @@ describe.skip("DoublyLinkedLists test suit", () => {
       expect(suit.get(2)?.prev?.value).toBe(3);
     });
     it("shift three time", () => {
-      suit.pushMuch([1, 2, 3, 4]);
       suit.shift();
       suit.shift();
       suit.shift();
@@ -61,7 +60,6 @@ describe.skip("DoublyLinkedLists test suit", () => {
       expect(suit.head?.value).toBe(4);
     });
     it("shift four time", () => {
-      suit.pushMuch([1, 2, 3, 4]);
       suit.shift();
       suit.shift();
       suit.shift();
@@ -71,40 +69,39 @@ describe.skip("DoublyLinkedLists test suit", () => {
       expect(suit.tail).toBe(null);
     });
   });
-  describe("unshifting should add new to the first list", () => {
+  describe("unshift should add new to the first list", () => {
+    beforeEach(() => {
+      [1, 2, 3, 4].forEach((num) => suit.push(num));
+    });
     it("unshifting one time", () => {
-      suit.pushMuch([1, 2, 3, 4]);
-      suit.unshifting(5);
+      suit.unshift(5);
       expect(suit.head?.value).toBe(5);
       expect(suit.head?.prev).toBe(null);
       expect(suit.get(2)?.value).toBe(1);
       expect(suit.get(2)?.prev?.value).toBe(5);
     });
     it("unshifting two time", () => {
-      suit.pushMuch([1, 2, 3, 4]);
-      suit.unshifting(5);
-      suit.unshifting(6);
+      suit.unshift(5);
+      suit.unshift(6);
       expect(suit.head?.value).toBe(6);
       expect(suit.head?.prev).toBe(null);
       expect(suit.get(2)?.value).toBe(5);
       expect(suit.get(2)?.prev?.value).toBe(6);
     });
     it("unshifting three time", () => {
-      suit.pushMuch([1, 2, 3, 4]);
-      suit.unshifting(5);
-      suit.unshifting(6);
-      suit.unshifting(7);
+      suit.unshift(5);
+      suit.unshift(6);
+      suit.unshift(7);
       expect(suit.head?.value).toBe(7);
       expect(suit.head?.prev).toBe(null);
       expect(suit.get(2)?.value).toBe(6);
       expect(suit.get(2)?.prev?.value).toBe(7);
     });
     it("unshifting four time", () => {
-      suit.pushMuch([1, 2, 3, 4]);
-      suit.unshifting(5);
-      suit.unshifting(6);
-      suit.unshifting(7);
-      suit.unshifting(8);
+      suit.unshift(5);
+      suit.unshift(6);
+      suit.unshift(7);
+      suit.unshift(8);
       expect(suit.head?.value).toBe(8);
       expect(suit.head?.prev).toBe(null);
       expect(suit.get(2)?.value).toBe(7);
@@ -112,41 +109,41 @@ describe.skip("DoublyLinkedLists test suit", () => {
     });
   });
   describe("get will return the right node base the index have put", () => {
+    beforeEach(() => {
+      [1, 2, 3, 4].forEach((num) => suit.push(num));
+    });
     it("get will return match node", () => {
-      suit.pushMuch([1, 2, 3, 4]);
       expect(suit.get(1)?.value).toBe(1);
       expect(suit.get(2)?.value).toBe(2);
       expect(suit.get(3)?.value).toBe(3);
       expect(suit.get(4)?.value).toBe(4);
     });
     it("error on get node out of range", () => {
-      suit.pushMuch([1, 2, 3, 4]);
       expect(() => suit.get(10)).toThrowErrorMatchingInlineSnapshot(
         '"10 is over  the node length 4"'
       );
     });
     it("error on get node withe order zero or lower", () => {
-      suit.pushMuch([1, 2, 3, 4]);
       expect(() => suit.get(0)).toThrowErrorMatchingInlineSnapshot(
         "\"node can't have order number 0 'Zero' or lower\""
       );
     });
   });
   describe("set should update the value of the given order  link list", () => {
+    beforeEach(() => {
+      [1, 2, 3, 4].forEach((num) => suit.push(num));
+    });
     it("set at first node", () => {
-      suit.pushMuch([1, 2, 3, 4]);
       suit.set(1, 10);
       expect(suit.head?.value).toBe(10);
       expect(suit.get(2)?.prev?.value).toBe(10);
     });
     it("set at last node", () => {
-      suit.pushMuch([1, 2, 3, 4]);
       suit.set(4, 10);
       expect(suit.tail?.value).toBe(10);
       expect(suit.get(4)?.value).toBe(10);
     });
     it("set at last node", () => {
-      suit.pushMuch([1, 2, 3, 4]);
       suit.set(2, 10);
       expect(suit.head?.next?.value).toBe(10);
       expect(suit.get(2)?.value).toBe(10);
@@ -155,7 +152,6 @@ describe.skip("DoublyLinkedLists test suit", () => {
   });
   describe("insert should add new node on specific order", () => {
     it("at zero order", () => {
-      suit.pushMuch([1, 2, 3, 4]);
       suit.insert(0, 12);
       expect(suit.head?.value).toBe(12);
       expect(suit.head?.prev).toBe(null);
@@ -164,7 +160,6 @@ describe.skip("DoublyLinkedLists test suit", () => {
       expect(suit.get(3)?.prev?.value).toBe(1);
     });
     it("at first order", () => {
-      suit.pushMuch([1, 2, 3, 4]);
       suit.insert(1, 12);
       expect(suit.head?.value).toBe(12);
       expect(suit.head?.prev).toBe(null);
@@ -173,7 +168,6 @@ describe.skip("DoublyLinkedLists test suit", () => {
       expect(suit.get(3)?.prev?.value).toBe(1);
     });
     it("at middle order", () => {
-      suit.pushMuch([1, 2, 3, 4]);
       suit.insert(2, 12);
       expect(suit.head?.value).toBe(1);
       expect(suit.head?.next?.value).toBe(12);
@@ -182,7 +176,6 @@ describe.skip("DoublyLinkedLists test suit", () => {
       expect(suit.get(3)?.prev?.value).toBe(12);
     });
     it("at last order", () => {
-      suit.pushMuch([1, 2, 3, 4]);
       suit.insert(10, 12);
       expect(suit.tail?.value).toBe(12);
       expect(suit.get(4)?.next?.value).toBe(12);
@@ -192,8 +185,10 @@ describe.skip("DoublyLinkedLists test suit", () => {
     });
   });
   describe("remove should eraser the node on specific order", () => {
+    beforeEach(() => {
+      [1, 2, 3, 4].forEach((num) => suit.push(num));
+    });
     it("remove first item", () => {
-      suit.pushMuch([1, 2, 3, 4]);
       expect(suit.remove(1)?.value).toBe(1);
       expect(suit.head?.value).toBe(2);
       expect(suit.head?.prev).toBe(null);
@@ -201,7 +196,6 @@ describe.skip("DoublyLinkedLists test suit", () => {
       expect(suit.get(2)?.prev?.value).toBe(2);
     });
     it("remove lastItem item", () => {
-      suit.pushMuch([1, 2, 3, 4]);
       suit.pop();
       expect(suit.tail?.value).toBe(3);
       expect(suit.tail?.prev?.value).toBe(2);
@@ -209,7 +203,6 @@ describe.skip("DoublyLinkedLists test suit", () => {
       expect(suit.get(3)?.prev?.value).toBe(2);
     });
     it("remove middle item", () => {
-      suit.pushMuch([1, 2, 3, 4]);
       expect(suit.remove(2)?.value).toBe(2);
       expect(suit.head?.value).toBe(1);
       expect(suit.head?.next?.value).toBe(3);
@@ -218,15 +211,12 @@ describe.skip("DoublyLinkedLists test suit", () => {
     });
   });
   it("reverse should reverse the link lists ", () => {
-    suit.pushMuch([1, 2, 3, 4]);
     suit.reverse();
     expect(suit.head?.value).toBe(4);
     expect(suit.get(2)?.value).toBe(3);
     expect(suit.get(3)?.value).toBe(2);
     expect(suit.get(4)?.value).toBe(1);
-    expect(suit.prettyPrint()).toMatchInlineSnapshot(
-      '"(4) <==> (3) <==> (2) <==> (1) <==> "'
-    );
+
     expect(suit.head?.prev).toBe(null);
     expect(suit.get(2)?.prev?.value).toBe(4);
     expect(suit.get(3)?.prev?.value).toBe(3);
@@ -237,36 +227,10 @@ describe.skip("DoublyLinkedLists test suit", () => {
     expect(suit.get(2)?.value).toBe(2);
     expect(suit.get(3)?.value).toBe(3);
     expect(suit.get(4)?.value).toBe(4);
-    expect(suit.prettyPrint()).toMatchInlineSnapshot(
-      '"(1) <==> (2) <==> (3) <==> (4) <==> "'
-    );
+
     expect(suit.head?.prev).toBe(null);
     expect(suit.get(2)?.prev?.value).toBe(1);
     expect(suit.get(3)?.prev?.value).toBe(2);
     expect(suit.get(4)?.prev?.value).toBe(3);
   });
-  it("should return the pretty print of linked list", () => {
-    suit.pushMuch([1, 2, 3, 4]);
-    expect(suit.prettyPrint()).toMatchInlineSnapshot(
-      '"(1) <==> (2) <==> (3) <==> (4) <==> "'
-    );
-  });
-});
-describe("component ", () => {
-  let t = new DoublyLinkedListSec();
-  it("test", () => {
-    [5, 10, 15, 20].forEach((num) => t.push(num));
-    console.log(t.reverse());
-    console.log(t.traverse());
-    console.log(t.length);
-    console.log(t?.head?.val);
-
-    console.log(t?.head?.next?.prev?.val);
-    console.log(t?.head?.next?.next?.val);
-    console.log(t?.head?.next?.next?.next?.val);
-  });
-  it.todo("should have some property ");
-  it.todo("should have some property ");
-  it.todo("should have some property ");
-  it.todo("should have some property ");
 });
--- src/DataStrucuture/DoublyLinkedLists.ts
+++ src/DataStrucuture/DoublyLinkedLists.ts
@@ -5,271 +5,14 @@
  *   and a pointer to another node or null, doubly liked list have
  *   next and previous pointer and make it easier to search
  */
-export class Nodes<T> {
-  next: Nodes<T> | null = null;
-  prev: Nodes<T> | null = null;
-  constructor(public value: T | number) {}
-}
-export class DoublyLinkedLists<T> {
-  public length: number = 0;
-  public head: Nodes<T> | null = null;
-  public tail: Nodes<T> | null = null;
-  constructor(value: T | null = null) {
-    this.push(value);
-  }
-  /** Push
-   * Double linked list push work almost same with the singly but the
-   * different we put the prev pointer first on new node
-   */
-  push(value: T | null) {
-    const NewNode = new Nodes(value);
-    if (value === null) return this;
-    if (!this.head) {
-      this.head = NewNode as Nodes<T>;
-      this.tail = this.head;
-      this.length++;
-      return this;
-    }
-    let currentNode = this.head;
-    while (currentNode) {
-      if (currentNode.next !== null) {
-        currentNode = currentNode.next;
-      } else {
-        currentNode.next = NewNode as Nodes<T>;
-        NewNode.prev = currentNode;
-        this.tail = currentNode.next;
-        this.length++;
-        return this;
-      }
-    }
-  }
-  pushMuch(arr: T[]) {
-    if (!arr.length) return this;
-    let value = arr.shift()!;
-    this.push(value);
-    this.pushMuch(arr);
-    return this;
-  }
-  /**pop
-   * on Double linked list to pop the last item, we just need to
-   * change the last item to the previous of the last item
-   */
-  pop() {
-    let currentTail = this.tail;
-    let longLists = this.length - 2;
-    if (longLists < 0) {
-      this.head = null;
-      this.tail = this.head;
-    } else if (currentTail?.prev) {
-      this.tail = currentTail.prev;
-      this.tail.next = null;
-    }
-    this.length--;
-    return this;
-  }
-  /**shift
-     *shift works by remove the first node on list with certain 
-      conditions if no link list return this if only one link 
-      list remove all change to the null if there is more then 
-      one change the first value to the next 
-     */
-  shift() {
-    let currentNode = this.head;
-    let longLists = this.length;
-    if (longLists <= 0) {
-      return this;
-    } else if (longLists === 1) {
-      this.head = null;
-      this.tail = this.head;
-      this.length--;
-      return this;
-    } else if (currentNode) {
-      currentNode = currentNode?.next;
-      currentNode!.prev = null;
-      this.head = currentNode;
-      this.length--;
-      return this;
-    }
-  }
-  /**Unshifting
-   * Adding a new node on the beginning of the linked list
-   */
-  unshifting(value: T) {
-    const NewNode = new Nodes(value);
-    let currentNode = this.head;
-    let longLists = this.length;
-    if (longLists < 0) {
-      this.head = NewNode;
-      this.tail = this.head;
-      this.length++;
-      return this;
-    } else {
-      if (currentNode?.prev === null) currentNode.prev = NewNode;
-      this.head = NewNode;
-      NewNode.next = currentNode;
-      this.length++;
-    }
-  }
-  /** get
-   * get use to return the node base on the number position,
-   * on the function we put num-1 on the indexNode we look
-   * because we start the loop count on 0 and we put the first
-   * list with length 1 so we need to put the equal comparison
-   * among the loop count and the list length so we can get the
-   * right node
-   */
-  get(num: number) {
-    const nodeLength = this.length;
-    if (num <= 0)
-      throw new Error("node can't have order number 0 'Zero' or lower");
-    else if (num > nodeLength)
-      throw new Error(`${num} is over  the node length ${nodeLength}`);
-    else if (num === nodeLength) return this.tail;
-    const indexNode = num - 1;
-    let currentNode = this.head;
-    let loopCount = 0;
-    while (currentNode) {
-      if (indexNode === loopCount) {
-        return currentNode;
-      }
-      currentNode = currentNode.next;
-      loopCount++;
-    }
-  }
-  /** set
-   * this function accept a value and index, use to update the value without
-   * change the order find specific node if the node is not found return
-   */
-  set(indx: number, value: T) {
-    const indexNode = this.get(indx);
-    if (!indexNode) throw new Error("no match index found");
-    else {
-      indexNode.value = value;
-      return indexNode;
-    }
-  }
-  /**Insert
-   * Adding a node to the linked lists at specific position
-   * if the index is lower then 0 or there is no node exist we
-   * put the value as new node, if the index is bigger then the
-   * node length we put the value as new node,if the index is
-   * one we use the value to unshift the node, then if then
-   * index is inside the range of node order we will looping
-   * to the two node before the match index so the next node
-   * of two node before can hold the new node and the new node
-   *  will hold then rest next node with the next pointer then
-   * the last one we increase the length
-   */
-  insert(indx: number, value: T) {
-    const NewNode = new Nodes(value);
-    const nodeLength = this.length;
-    if (nodeLength === 0) {
-      this.head = NewNode;
-      this.tail = this.head;
-      this.length++;
-      return this;
-    } else if (indx > nodeLength) {
-      const lastNode = this.get(nodeLength);
-      if (lastNode?.next === null) {
-        NewNode.prev = lastNode;
-        lastNode.next = NewNode;
-        this.tail = NewNode;
-        this.length++;
-      }
-    } else if (indx <= 1) {
-      const InsertOnFirstNode = this.unshifting(value);
-      return InsertOnFirstNode;
-    } else {
-      const currentNode = this.get(indx - 1);
-      let backPointer = currentNode;
-      let frondPointer = currentNode?.next!;
-      if (backPointer?.next) backPointer.next = NewNode;
-      NewNode.prev != backPointer;
-      NewNode.next = frondPointer;
-      frondPointer.prev = NewNode;
-      this.length++;
-      return NewNode;
-    }
-  }
-  /** Remove
-   * Remove use to remove the node on specific position,
-   * if the index is smaller then list length we unshift the
-   * node, if the index is greater then the list length we pop
-   *  the node, if the index is in the range of the lists we
-   * loop the node until the index-1 then we remove the
-   * indexNode by put the beckNext pointer to the frontNext
-   * pointer
-   */
-  remove(index: number) {
-    if (index > this.length && this.tail?.prev) {
-      const lastNode = this.tail;
-      this.pop();
-      return lastNode;
-    } else if (index <= 1) {
-      const currentHead = this.head;
-      this.shift();
-      return currentHead;
-    } else {
-      const currentNode = this.get(index - 1);
-      if (currentNode) {
-        const removeNode = currentNode?.next;
-        const frontPointer = removeNode?.next!;
-        if (currentNode?.next) currentNode.next = frontPointer;
-        frontPointer.prev = currentNode;
-        this.length--;
-        return removeNode;
-      }
-    }
-  }
-  /**Reverse
-     *Reverse works by build some End node to put on the next 
-      pointer next node then the first node already modifier put 
-      the Custom variable, then the cycle will repeat until the 
-      last pinter and all the list get reverse because all the 
-      prevNode get put on the next current node  
-  
-      for reverse the list on the class we put several addition 
-      where we put the head value on tail first then we change 
-      the reverse result as the head  
-     */
-  reverse() {
-    if (!this.head) return this;
-    if (this.length <= 1) return this;
-    let currentNode = this.head;
-    let TempNode: null | Nodes<T> = null;
-    let previousHead = this.head;
-    previousHead?.next != null;
-    this.tail = previousHead;
-    while (currentNode) {
-      let NextNode = currentNode?.next!;
-      currentNode.prev = currentNode.next!;
-      currentNode!.next = TempNode;
-      TempNode = currentNode;
-      currentNode = NextNode;
-    }
-    this.head = TempNode;
-    return this;
-  }
-  prettyPrint() {
-    let currentNode = this.head;
-    let PrettyPrint = "";
-    while (currentNode) {
-      let values = currentNode?.value;
-      PrettyPrint += `(${values}) <==> `;
-      currentNode = currentNode.next;
-    }
-    return PrettyPrint;
-  }
-}
+
 class Node {
   public next: Node | null = null;
   public prev: Node | null = null;
-  constructor(public val: number) {
-    this.val = val;
-  }
+  constructor(public value: number) {}
 }
 
-export class DoublyLinkedListSec {
+export class DoublyLinkedList {
   public head: Node | null = null;
   public tail: Node | null = null;
   public length: number = 0;
@@ -317,6 +60,12 @@ export class DoublyLinkedListSec {
       index--;
     }
     return currentNode;
+  }
+  set(index: number, value: number) {
+    if (index > this.length) return false;
+    let node = this.get(index);
+    node!.value = value;
+    return true;
   }
   remove(index: number) {
     if (index === 0) {
@@ -403,7 +152,7 @@ export class DoublyLinkedListSec {
   }
   traverse(node = this.head, list: number[] = []): number[] {
     if (!node) return list;
-    list.push(node.val);
+    list.push(node.value);
     return this.traverse(node.next, list);
   }
 }
